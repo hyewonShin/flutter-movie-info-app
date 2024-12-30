47ad252d928602f2cf3cdbcfc85399b78efd3b04
hyewonShin <67300338+hyewonShin@users.noreply.github.com>
2024-12-30 21:34:18 +0900

    feat: MovieListViewModel 테스트 코드 작성

---
+++ test/presentation/movie_list_view_model_test.dart
@@ -0,0 +1,52 @@
+import 'package:flutter_movie_app/domain/entity/movie.dart';
+import 'package:flutter_movie_app/domain/usecase/fetch_movies_usecase.dart';
+import 'package:flutter_movie_app/presentation/%08providers.dart';
+import 'package:flutter_movie_app/presentation/pages/homepage/movie_list_view_model.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+
+class MockFetchMoviesUsecase extends Mock implements FetchMoviesUsecase {}
+
+void main() {
+  late final ProviderContainer providerContainer;
+
+  setUp(
+    () async {
+      final fetchMoviesUsecaseProviderOverride =
+          fetchMoviesUsecaseProvider.overrideWith(
+        (ref) => MockFetchMoviesUsecase(),
+      );
+      providerContainer = ProviderContainer(overrides: [
+        fetchMoviesUsecaseProviderOverride,
+      ]);
+      addTearDown(providerContainer.dispose);
+    },
+  );
+
+  test(
+    'MovieListViewModel test : state update after fetchMovies',
+    () async {
+      const testId = 1; // Example test id value
+      final fetchMoviesUseCase =
+          providerContainer.read(fetchMoviesUsecaseProvider);
+      when(() => fetchMoviesUseCase.execute()).thenAnswer((_) async => [
+            Movie(
+              id: testId,
+              poster_path: 'poster_path',
+            )
+          ]);
+
+      final stateBefore = providerContainer.read(movieListViewModelProvider);
+      expect(stateBefore, isNull);
+      await providerContainer
+          .read(movieListViewModelProvider.notifier)
+          .fetchMovies();
+
+      final stateAfter = providerContainer.read(movieListViewModelProvider);
+      expect(stateAfter, isNotNull);
+      expect(stateAfter!.length, 1);
+      expect(stateAfter[0].id, 'poster_path');
+    },
+  );
+}
