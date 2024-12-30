62af57d882c38f4fef292b116a2683584d6089ff
hyewonShin <67300338+hyewonShin@users.noreply.github.com>
2024-12-30 21:33:04 +0900

    feat: FetchMoviesUsecase 클래스 생성

---
+++ lib/domain/usecase/fetch_movies_usecase.dart
@@ -0,0 +1,12 @@
+import 'package:flutter_movie_app/domain/entity/movie.dart';
+import 'package:flutter_movie_app/domain/repository/movie_repository.dart';
+
+class FetchMoviesUsecase {
+  FetchMoviesUsecase(this._movieRepository);
+
+  final MovieRepository _movieRepository;
+
+  Future<List<Movie>> execute() async {
+    return await _movieRepository.fetchMovies();
+  }
+}
