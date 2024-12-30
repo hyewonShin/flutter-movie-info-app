b44108974146841ae39f6b5895bb8c461e466222
hyewonShin <67300338+hyewonShin@users.noreply.github.com>
2024-12-30 21:33:20 +0900

    feat: FetchMoviesUsecase 테스트 코드 작성

---
+++ test/domain/fetch_movies_usecase_test.dart
@@ -0,0 +1,35 @@
+import 'package:flutter_movie_app/domain/entity/movie.dart';
+import 'package:flutter_movie_app/domain/repository/movie_repository.dart';
+import 'package:flutter_movie_app/domain/usecase/fetch_movies_usecase.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+
+class MockMovieRepository extends Mock implements MovieRepository {}
+
+void main() {
+  late final MockMovieRepository mockMovieRepository;
+  late final FetchMoviesUsecase fetchMoviesUsecase;
+  setUp(
+    () async {
+      mockMovieRepository = MockMovieRepository();
+      fetchMoviesUsecase = FetchMoviesUsecase(mockMovieRepository);
+    },
+  );
+
+  test(
+    'FetchMoviesUsecase test : fetchMovies',
+    () async {
+      const testId = 1;
+      when(() => mockMovieRepository.fetchMovies()).thenAnswer((_) async => [
+            Movie(
+              id: testId,
+              poster_path: 'poster_path',
+            )
+          ]);
+
+      final result = await fetchMoviesUsecase.execute();
+      expect(result.length, 1);
+      expect(result[0].id, 'id');
+    },
+  );
+}
