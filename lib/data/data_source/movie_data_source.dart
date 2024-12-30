55638e4eb6eda40823979b3670b7b9ee957cb815
hyewonShin <67300338+hyewonShin@users.noreply.github.com>
2024-12-30 21:27:59 +0900

    feat: MovieDataSource 클래스 생성

---
+++ lib/data/data_source/movie_data_source.dart
@@ -0,0 +1,18 @@
+import 'package:flutter_movie_app/data/dto/movie_detail_dto.dart';
+import 'package:flutter_movie_app/data/dto/movie_dto.dart';
+
+// 추상 클래스로서 객체를 직접 생성할 수 없으며, 주로 상속을 통해 다른 클래스가 구현해야 하는 메서드를 정의
+
+abstract interface class MovieDataSource {
+  Future<List<MovieDto>> fetchMovies();
+
+  // Future<MovieDto?> fetchNowPlayingMovies();
+
+  // Future<MovieDto?> fetchPopularMovies();
+
+  // Future<MovieDto?> fetchTopRatedMovies();
+
+  // Future<MovieDto?> fetchUpcomingMovies();
+
+  // Future<MovieDetailDto?> fetchMovieDetail(int id);
+}
