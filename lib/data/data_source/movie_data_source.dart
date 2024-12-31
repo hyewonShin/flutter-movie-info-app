1b0c80a8c337f52e5f000543c60c80421376354a
hyewonShin <67300338+hyewonShin@users.noreply.github.com>
2024-12-31 13:31:40 +0900

    feat: data_sourece에 카테고리별 fetch 메서드 추가

--- lib/data/data_source/movie_data_source.dart
+++ lib/data/data_source/movie_data_source.dart
@@ -4,15 +4,13 @@ import 'package:flutter_movie_app/data/dto/movie_dto.dart';
 // 추상 클래스로서 객체를 직접 생성할 수 없으며, 주로 상속을 통해 다른 클래스가 구현해야 하는 메서드를 정의
 
 abstract interface class MovieDataSource {
-  Future<List<MovieDto>> fetchMovies();
+  Future<List<MovieDto>> fetchNowPlayingMovies();
 
-  // Future<MovieDto?> fetchNowPlayingMovies();
+  Future<List<MovieDto>> fetchPopularMovies();
 
-  // Future<MovieDto?> fetchPopularMovies();
+  Future<List<MovieDto>> fetchTopRatedMovies();
 
-  // Future<MovieDto?> fetchTopRatedMovies();
+  Future<List<MovieDto>> fetchUpcomingMovies();
 
-  // Future<MovieDto?> fetchUpcomingMovies();
-
-  // Future<MovieDetailDto?> fetchMovieDetail(int id);
+  Future<MovieDetailDto?> fetchMovieDetail(int id);
 }
