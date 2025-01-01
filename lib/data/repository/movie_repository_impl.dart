4c26dbc133f0fbe8f5a594340ef896759904500d
hyewonShin <67300338+hyewonShin@users.noreply.github.com>
2025-01-02 01:13:47 +0900

    feat: movie_repository_impl 클래스에 카테고리별 출력 및 상세데이터 출력 메서드 추가

--- lib/data/repository/movie_repository_impl.dart
+++ lib/data/repository/movie_repository_impl.dart
@@ -1,5 +1,6 @@
 import 'package:flutter_movie_app/data/data_source/movie_data_source.dart';
 import 'package:flutter_movie_app/domain/entity/movie.dart';
+import 'package:flutter_movie_app/domain/entity/movie_detail.dart';
 import 'package:flutter_movie_app/domain/repository/movie_repository.dart';
 
 class MovieRepositoryImpl implements MovieRepository {
@@ -18,5 +19,58 @@ class MovieRepositoryImpl implements MovieRepository {
           ),
         )
         .toList();
+  }
+
+  @override
+  Future<List<Movie>> fetchPopularMovies() async {
+    final result = await _movieDataSource.fetchPopularMovies();
+    // domain 의 엔티티 모델로 매핑
+    return result
+        .map(
+          (e) => Movie(
+            id: e.id,
+            poster_path: e.poster_path,
+          ),
+        )
+        .toList();
+  }
+
+  @override
+  Future<List<Movie>> fetchTopRatedMovies() async {
+    final result = await _movieDataSource.fetchTopRatedMovies();
+    // domain 의 엔티티 모델로 매핑
+    return result
+        .map(
+          (e) => Movie(
+            id: e.id,
+            poster_path: e.poster_path,
+          ),
+        )
+        .toList();
+  }
+
+  @override
+  Future<List<Movie>> fetchUpcomingMovies() async {
+    final result = await _movieDataSource.fetchUpcomingMovies();
+    // domain 의 엔티티 모델로 매핑
+    return result
+        .map(
+          (e) => Movie(
+            id: e.id,
+            poster_path: e.poster_path,
+          ),
+        )
+        .toList();
+  }
+
+  @override
+  Future<MovieDetail> fetchMovieDetail(int id) async {
+    final result = await _movieDataSource.fetchMovieDetail(id);
+    // domain 의 엔티티 모델로 매핑
+    if (result != null) {
+      return MovieDetail.fromDTO(result.toJson());
+    } else {
+      throw Exception('Failed to fetch movie details');
+    }
   }
 }
