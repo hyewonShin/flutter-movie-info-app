810398ac69d76c6c3bdd70bce8ce48dba56b66c2
hyewonShin <67300338+hyewonShin@users.noreply.github.com>
2025-01-02 01:15:41 +0900

    feat: fetch_movies_usecase 클래스에 카테고리 별 출력 및 상세데이터 출력 메서드 추가

--- lib/domain/usecase/fetch_movies_usecase.dart
+++ lib/domain/usecase/fetch_movies_usecase.dart
@@ -1,4 +1,5 @@
 import 'package:flutter_movie_app/domain/entity/movie.dart';
+import 'package:flutter_movie_app/domain/entity/movie_detail.dart';
 import 'package:flutter_movie_app/domain/repository/movie_repository.dart';
 
 class FetchMoviesUsecase {
@@ -6,7 +7,24 @@ class FetchMoviesUsecase {
 
   final MovieRepository _movieRepository;
 
-  Future<List<Movie>> execute() async {
+  Future<List<Movie>> fetchNowPlayingMovies() async {
     return await _movieRepository.fetchNowPlayingMovies();
+  }
+
+  Future<List<Movie>> fetchPopularMovies() async {
+    return await _movieRepository.fetchPopularMovies();
+  }
+
+  Future<List<Movie>> fetchTopRatedMovies() async {
+    return await _movieRepository.fetchTopRatedMovies();
+  }
+
+  Future<List<Movie>> fetchUpcomingMovies() async {
+    return await _movieRepository.fetchUpcomingMovies();
+  }
+
+  Future<MovieDetail?> fetchMovieDetail(int id) async {
+    var result = await _movieRepository.fetchMovieDetail(id);
+    return result;
   }
 }
