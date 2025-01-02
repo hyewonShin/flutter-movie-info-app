560852338d5f73936f8784807abfb4f88b4ad924
hyewonShin <67300338+hyewonShin@users.noreply.github.com>
2025-01-02 22:29:09 +0900

    refactor: 파라미터 변수명 text => genre로 변경

--- lib/presentation/pages/detailpage/widget/movie_category.dart
+++ lib/presentation/pages/detailpage/widget/movie_category.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
 
-Container MovieCategory(String text) {
+Container MovieCategory(String genre) {
   return Container(
     padding: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
     decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(30),
         border: Border.all(color: Colors.white)),
     child: Text(
-      text,
+      genre,
       style: TextStyle(color: Colors.blue),
     ),
   );
